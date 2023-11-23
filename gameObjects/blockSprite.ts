979d0b63571ec7f59646d66b3d49321c6d8e9be6
RouRai <k1215050@students.katyisd.org>
2023-11-22 22:33:03 -0600

    added many more things for blockSprite.ts

--- gameObjects/blockSprite.ts
+++ gameObjects/blockSprite.ts
@@ -4,26 +4,37 @@ export class BlockSprite {
     private readonly _yPosition: number;
     private readonly _width: number;
     private readonly _height: number;
+    private readonly _windowWidth: number;
+    private readonly _windowHeight: number;
     private readonly _color: string;
     private readonly _context: CanvasRenderingContext2D;
 
-    constructor(startXPosition: number, startYPosition: number, width: number, height: number, color: string, context: CanvasRenderingContext2D) {
+    constructor(startXPosition: number, startYPosition: number, width: number, height: number,
+                windowWidth: number, windowHeight: number, color: string,
+                context: CanvasRenderingContext2D) {
         this._xPosition = startXPosition;
         this._yPosition = startYPosition;
         this._width = width;
         this._height = height;
+        this._windowWidth = windowWidth;
+        this._windowHeight = windowHeight;
         this._color = color;
         this._context = context;
     }
 
-    draw(context: CanvasRenderingContext2D = this.context) {
-        context.beginPath();
-        context.strokeStyle = this.color;
-        context.fillStyle = this.color;
-        context.fillRect(this.xPosition, this.yPosition, this.width, this.height)
-        context.stroke();
-        context.fill()
-        context.closePath();
+    draw() {
+        this.context.beginPath();
+        this.context.strokeStyle = this.color;
+        this.context.fillStyle = this.color;
+        this.context.fillRect(this.xPosition, this.yPosition, this.width, this.height)
+        this.context.stroke();
+        this.context.fill()
+        this.context.closePath();
+    }
+
+    update() {
+        this.context.clearRect(0, 0, this.windowWidth, this.windowHeight);
+        this.draw();
     }
 
     get xPosition(): number {
@@ -40,6 +51,14 @@ export class BlockSprite {
 
     get height(): number {
         return this._height;
+    }
+
+    get windowWidth(): number {
+        return this._windowWidth;
+    }
+
+    get windowHeight(): number {
+        return this._windowHeight;
     }
 
     get color(): string {
