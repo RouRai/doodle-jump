93cd1ced5d4743d93ffa5e0bc63da9aab7a5ccee
RouRai <k1215050@students.katyisd.org>
2023-11-22 22:33:14 -0600

    added many more things for index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
-import {
-    Platform
-
-} from './gameObjects/platform'
+import { Platform } from './gameObjects/platform'
+import { BlockSprite } from "./gameObjects/blockSprite";
 
 let canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
@@ -13,3 +11,41 @@ let windowHeight = window.innerHeight;
 canvas.width = windowWidth;
 canvas.height = windowHeight;
 
+let platformOne = new Platform(
+    250,
+    250,
+    150,
+    50,
+    windowWidth,
+    windowHeight,
+    50,
+    context
+);
+
+platformOne.draw();
+
+let blockSprite = new BlockSprite(
+    0,
+    0,
+    50,
+    50,
+    windowWidth,
+    windowHeight,
+    "black",
+    context
+);
+
+blockSprite.draw();
+
+let updateBlockSprite = function () {
+    requestAnimationFrame(blockSprite.update);
+    blockSprite.update();
+}
+
+let updatePlatform = function () {
+    requestAnimationFrame(platformOne.update);
+    platformOne.update();
+}
+
+updateBlockSprite();
+updatePlatform();
